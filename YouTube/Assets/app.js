e5b4ea40ff9f524b1d9bc9d366dedef8e9aa4a8f
MuhammadHuzaifa22 <153933649+MuhammadHuzaifa22@users.noreply.github.com>
2024-05-26 23:54:00 +0500

    Work Completed

---
+++ YouTube/Assets/app.js
@@ -0,0 +1,444 @@
+const products = [
+    // Cricket
+    {
+        name: "2nd T20i Highlights | Pakistan Tour Of England | 25th May 2024",
+        views: '554K views 14 hours ago',
+        category: "Cricket",
+        channel: "Sony Liv",
+        image : src = "./Assets/Images/all-7image.jpg",
+        imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "Buutler Hits 84 & Archer takes 2-fer | Highlights England v Pakistan 2nd Men T201..",
+        views: '2.8M views 13 hour ago',
+        category: "Cricket",
+        channel: "England & Wales Cricket Board",
+        image : src = "./Assets/Images/But.jpg",
+        imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "Fakhar Zaman Press Confress Pakistan vs England T20i",
+        views: '51K views 1 day ago',
+        category: "Cricket",
+        channel: "Pakistan Cricket",
+        image : src = "./Assets/Images/all-6image.jpg",
+        imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "Chase Smact 67 of 38 | Highlights | West Indies vs South Africa | 2nd T20i",
+        views: '269K views 11 months ago',
+        category: "Cricket",
+        channel: "Windies Cricket",
+        image : src = "./Assets/Images/cric4Th.jpg",
+        imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "The Best Yorkers of the 2019  CWC! | Unplayable Delieveries | ICC cricket World Cup 2019",
+        views: '35M views 4 years ago',
+        category: "Cricket",
+        channel: "ICC",
+        image : src = "./Assets/Images/starc.jpg",
+        imagetwo : src = "./Assets/Images/avatar.png"
+    },
+
+    {
+        name: "Jos Buttler And Alex Hales smash record partnership | Semi final 2 |...",
+        views: '267K views 2 days ago',
+        category: "Cricket",
+        channel: "ICC",
+        image : src = "./Assets/Images/jos.jpg",
+        imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "Training Action as the boys focus on the england series #ENGvPAK",
+        views: '73K views 7 days ago',
+        category: "Cricket",
+        channel: "Pakistan Cricket",
+       image : src = "./Assets/Images/riz.jpg",
+       imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "Rajhastan Royals v Royal Challengers Bengaluru | 2nd Innings",
+        views: '98K views 3 days ago',
+        category: "Cricket",
+        channel: "tapmad",
+         image : src = "./Assets/Images/rr.jpg",
+         imagetwo : src = "./Assets/Images/avatar.png"
+    },
+
+
+
+    // Safari
+    {
+        name: "Asiatic Lions,only in Gir National Park,Gujarat,India",
+        views: '23K views 3 months agp',
+        category: "Safari",
+        channel: "We Love Switzerland",
+            image : src = "./Assets/Images/all-3image.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "African Safari 4K | Incredible Big five animal Sightings(Kruger National Park)",
+        views: '1.3M views 6 months ago',
+        category: "Safari",
+        channel: "the luxury travel expert",
+            image : src = "./Assets/Images/sf1.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "26 Incridible Safari Moments Caught on Camera",
+        views: '6.9M views 3 years ago',
+        category: "Safari",
+        channel: "Newsflare",
+            image : src = "./Assets/Images/two.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "African Safari 4K-Amazing Wildlife of African Savanna | Scenic Relaxation Film",
+        views: '2M views 11 months ago',
+        category: "Safari",
+        channel: "Scenic Scenes",
+            image : src = "./Assets/Images/h7.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "African Safari 4K-Scenic Wildlife Film With African Music",
+        views: '17M views 2 years ago',
+        category: "Safari",
+        channel: "Scenic Relaxation",
+            image : src = "./Assets/Images/af2.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "30 times african Safari Trips Went Horribally Wrong",
+        views: '4.3M views 1 year ago',
+        category: "Safari",
+        channel: "Claws",
+            image : src = "./Assets/Images/af3.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "Kanha National Park;Kanha Tiger Reserve India Part 4 of India and Srilanka Trip",
+        views: '3.2M views 3 years ago',
+        category: "Safari",
+        channel: "James Rattray",
+            image : src = "./Assets/Images/agf4.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+
+    {
+        name: "Blippi Explores a Safari Park | Learn About Animals For Kids | Educational Videos For Toddlers",
+        views: '4.4M views 2 years ago',
+        category: "Safari",
+        channel: "blippi- Educational Videos For Kids",
+            image : src = "./Assets/Images/bl1.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+
+    // Babar Azam
+    {
+        name: "Leeds Babar Azam Classic Shots before 1rst T20 Pakistan vs England | Pak..",
+        views: '37K views 5 day ago',
+        category: "Babar Azam",
+        channel: "BBN SPORTS",
+            image : src = "./Assets/Images/ba1.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "Babar Azam's Pre Series Press Confress | Pakistan vs England T20Is,2024|PCB|MA2A",
+        views: '117K view 5 days ago',
+        category: "Babar Azam",
+        channel: "Pakistan Cricket",
+            image : src = "./Assets/Images/ba2.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "Classy Batting By Babar Azam | Karachi Kings vs Peshawar Zamli | Match 29 | HBL PSL 9| M1Z2U",
+        views: '29K views 2 months ago',
+        category: "Babar Azam",
+        channel: "Pakistan Super Leauge",
+            image : src = "./Assets/Images/ba3.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "Babar Azam Press Confrence Karachi Kings Vs Peshawar Zalmi Match 29",
+        views: '53K views Streamed 2 months ago',
+        category: "Babar Azam",
+        channel: "Pakistan Super Leauge",
+            image : src = "./Assets/Images/ba4.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "Babar Azam Slow Batting Doomed The Pakistan Cricket Team | Pak vs Eng| Salman butt dig Deeps",
+        views: '4.2K views 4 hours ago',
+        category: "Babar Azam",
+        channel: "Hafzi Mohammad Imran Official",
+            image : src = "./Assets/Images/ba8.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "Babar Azam 58 Against England 2021 Highlights",
+        views: 29.99,
+        category: "Babar Azam",
+        channel: "Ms Cricket",
+            image : src = "./Assets/Images/fd1.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "Show Time With Ramiz Raja | Babar Azam|11 MAy 2024|Ep 21 Digitally Powered by zeera plus",
+        views: '923K views 2 weeks ago',
+        category: "Babar Azam",
+        channel: "ShowTime With Ramiz Raja",
+            image : src = "./Assets/Images/bs1.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "Imad Waism Spoke In Favour Of Babar AzaM Captancy",
+        views: '6.8K views 4 hours ago',
+        category: "Babar Azam",
+        channel: "GEO Super",
+            image : src = "./Assets/Images/immi1.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+
+    // National Geographic
+    {
+        name: "10 times hyena steal meals from big Cats like lion,leopards and cheetahs.",
+        views: '62 views 1 day ago',
+        category: "National Geographic",
+        channel: "wildlife world",
+            image : src = "./Assets/Images/pr3.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "Predators Attack Wildlife | Wildlife documentry in hindi",
+        views: '7.4M views 1 year ago',
+        category: "National Geographic",
+        channel: "Wildlife Telecast",
+            image : src = "./Assets/Images/pr2.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "Wolves V/S The Bison|America's National Parks|Full Episode|s-1 E-6 National Geographic",
+        views: '39K views',
+        category: "National Geographic",
+        channel: "National Geographic India",
+            image : src = "./Assets/Images/pr4.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "Botswana's Green Ecosystem | Africa's Deadliest | Full Episode | S07-E01 | हिंदी | #NatGeoIndia",
+        views: '5.4M views 8 months ago',
+        category: "National Geographic",
+        channel: "National Geographic India",
+            image : src = "./Assets/Images/mt1.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "Deadly Snakes - Canada and Indonesia, हिन्दी डॉक्यूमेंट्री | Wildlife documentary in Hindi",
+        views: '207K views 11 months ago',
+        category: "National Geographic",
+        channel: "Wildlife Telecast",
+            image : src = "./Assets/Images/sn1.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "The Panther - हिन्दी डॉक्यूमेंट्री | Discovery channel documentary in Hindi",
+        views: '527K views 10 months ago',
+        category: "National Geographic",
+        channel: "National Geographic India",
+            image : src = "./Assets/Images/d.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "Animals of Amazon 4K - Animals That Call The Jungle Home | Amazon Rainforest |Scenic Relaxation Film",
+        views: '26M views 1 year ago',
+        category: "National Geographic",
+        channel: "Scenic Scenes",
+            image : src = "./Assets/Images/sd.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "Creatures of the Canyon | America’s National Parks | हिन्दी | Full Episode | S1-E1 | Nat Geo",
+        views: '10K views 5 hours ago',
+        category: "National Geographic",
+        channel: "National Geographic India",
+            image : src = "./Assets/Images/g1.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+
+    // News
+    {
+        name: "Geo News Headlines 11 AM - Veteran actor Talat Hussain passes away | 26 May 2024",
+        views: '53K views 7 hours ago',
+        category: "News",
+        channel: "Geo News",
+            image : src = "./Assets/Images/32.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "Samaa News Headlines 2PM | 25 May 2024 | SAMAA TV",
+        views: '20K views 1 day ago',
+        category: "News",
+        channel: "SAMAAA TV",
+            image : src = "./Assets/Images/nh.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "ARY News 6 AM Headlines 25th May 2024 | PTI's Victory Announcement - Big News",
+        views: '107K views 1 daty ago',
+        category: "News",
+        channel: "ARY News",
+            image : src = "./Assets/Images/asa.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "Good News! MET Department Prediction About Rain | Latest Weather Update News | Samaa TV",
+        views: '55K views 1 day ago',
+        category: "News",
+        channel: "SAMAA TV",
+            image : src = "./Assets/Images/ff.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "Extreme Hot Weather In Pakistan | Heat Wave Alert | Latest Update | SAMAA TV",
+        views: '8.4K views 5 hours ago',
+        category: "News",
+        channel: "SAMAA TV",
+            image : src = "./Assets/Images/fgf.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "Extreme heat wave in Karachi - Latest Weather Updates",
+        views: '37K views 1 day ago',
+        category: "News",
+        channel: "ARY NEWS",
+            image : src = "./Assets/Images/45.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "Chances of rain in different areas - Latest weather updates",
+        views: '117K views 4 days ago',
+        category: "News",
+        channel: "ARY NEWS",
+            image : src = "./Assets/Images/hs.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+    {
+        name: "MET Prediction About Rain | Weather Update | Breaking News | SAMAA TV",
+        views: '26K views 9 hours ago',
+        category: "News",
+        channel: "SAMAA TV",
+            image : src = "./Assets/Images/sds.jpg",
+            imagetwo : src = "./Assets/Images/avatar.png"
+    },
+
+
+
+
+    // Gaming
+    {
+        name: "OUR PLANE IS HIJACK | CALL OF DUTY MODERN WARFARE III GAMEPLAY #3",
+        views: '16M views',
+        category: "Gaming",
+        channel: "Tecno Gamerz",
+            image : src = "./Assets/Images/fg.jpg",
+                    imagetwo : src = "./Assets/Images/avatar.png"
+
+    },
+    {
+        name: "WORKING IN A HORROR GRAVEYARD AT NIGHT",
+        views: '6.1M views 9 days ago',
+        category: "Gaming",
+        channel: "Tecno Gamerz",
+            image : src = "./Assets/Images/sdg.jpg",
+                    imagetwo : src = "./Assets/Images/avatar.png"
+
+    },
+    {
+        name: "Assets/Images/sdg.jpg",
+        views: '87M views 10 months ago',
+        category: "Gaming",
+        channel: "MR Beast",
+            image : src = "./Assets/Images/mnn.jpg",
+                    imagetwo : src = "./Assets/Images/avatar.png"
+
+    },
+    {
+        name: "MINECRAFT SPEEDRUNNER VS HUNTERS",
+        views: '11M views 2 years ago',
+        category: "Gaming",
+        channel: "Tecno Gamerz",
+            image : src = "./Assets/Images/tc.jpg",
+                    imagetwo : src = "./Assets/Images/avatar.png"
+
+    },
+];
+
+// console.log(products);
+
+let card = document.querySelector('#card-section');
+
+products.map(item => {
+    // console.log(item);
+    card.innerHTML += `<div class="for-Row">
+    <div class="image_sec">
+    <img src="${item.image}">
+    </div>
+    <div class="iMg">
+    <img src="${item.imagetwo}">
+    <h6 class="card-title">${item.name}</h6>        
+    </div>
+    <p id="STy">${item.channel}</p>
+    <p id="STr">${item.views}</p>
+
+    </div>
+</div>
+`
+})
+
+function filterdItems(btn) {
+    card.innerHTML = ''
+    console.log(btn.textContent);
+    let filterdItems = products.filter(item => item.category === btn.innerHTML).map(item => {
+        card.innerHTML +=  `<div class="for-Row">
+        <div class="image_sec">
+        <img src="${item.image}">
+        </div>
+        <div class="iMg">
+        <img src="${item.imagetwo}">
+        <h6 class="card-title">${item.name}</h6>        
+        </div>
+        <p id="STy">${item.channel}</p>
+    <p id="STr">${item.views}</p>
+
+        </div>
+    </div>
+    `
+        return item
+    })
+    console.log(filterdItems);
+}
+
+function allItems(){
+    card.innerHTML = ''
+    
+    products.map(item => {
+        // console.log(item);
+        card.innerHTML +=  `<div class="for-Row">
+        <div class="image_sec">
+        <img src="${item.image}">
+        </div>
+        <div class="iMg">
+        <img src="${item.imagetwo}">
+        <h6 class="card-title">${item.name}</h6>        
+        </div>
+        <p id="STy">${item.channel}</p>
+        <p id="STr">${item.views}</p>
+       
+        </div>
+
+    </div>
+    `
+    })
+}
